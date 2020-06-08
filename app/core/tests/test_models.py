7581821e9b83767c5b21bbc6c4ae31b57028623d
Fawzi Al Masri <fawzi@Fawzis-MacBook-Pro.local>
2020-06-08 23:07:51 +0200

    Creating Core App and adding User Model

---
+++ app/core/tests/test_models.py
@@ -0,0 +1,22 @@
+from django.test import TestCase
+from django.contrib.auth import get_user_model
+
+class ModelTests(TestCase):
+    def test_create_user_email_with_successful(self):
+        """Creating a new user with an email is successfull"""
+        email = 'bloodvoid10@hotmail.com'
+        password = 'Testpass123'
+        user = get_user_model().objects.create_user(
+            email=email,
+            password=password
+        )
+
+        self.assertEqual(user.email, email)
+        self.assertTrue(user.check_password(password))
+
+    def test_new_user_email_normalized(self):
+        """Test the that the new email for the user is normalized"""
+        email = 'bloodvoid10@hotmail.com'
+        user = get_user_model().objects.create_user(email, 'test123')
+
+        self.assertEqual(user.email, email.lower())
