5ce028c1738f2484edb96ca8d7b1ea85a21098e3
Fawzi Al Masri <fawzi@Fawzis-MacBook-Pro.local>
2020-06-09 00:47:42 +0200

    Bugfixes

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -2,6 +2,8 @@ from django.test import TestCase
 from django.contrib.auth import get_user_model
 
 class ModelTests(TestCase):
+
+    
     def test_create_user_email_with_successful(self):
         """Creating a new user with an email is successfull"""
         email = 'bloodvoid10@hotmail.com'
