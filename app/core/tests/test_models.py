696d62c39db48dc5804329917df638ff3dee4af7
Fawzi Al Masri <fawzi@Fawzis-MacBook-Pro.local>
2020-06-09 22:33:55 +0200

    Indentation fixed #6

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -4,7 +4,6 @@ from django.contrib.auth import get_user_model
 
 class ModelTests(TestCase):
 
-
     def test_create_user_email_with_successful(self):
         """Creating a new user with an email is successfull"""
         email = 'bloodvoid10@hotmail.com'
@@ -14,10 +13,8 @@ class ModelTests(TestCase):
             password=password
         )
 
-
         self.assertEqual(user.email, email)
         self.assertTrue(user.check_password(password))
-
 
     def test_new_user_email_normalized(self):
         """Test the that the new email for the user is normalized"""
